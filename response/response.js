c3627d32480365b765130c7281c9afb1c6f3f593
vinanamira <mzssdaru@gmail.com>
2024-03-03 16:10:44 +0700

    Implement Sequelize

---
+++ response/response.js
@@ -0,0 +1,24 @@
+const sendResponse = (res, statusCode, message, data = null) => res.status(statusCode).json({
+    statusCode,
+    message,
+    data,
+});
+
+const sendError = (res, message, status = 500) => res.status(status).json({
+    status,
+    message,
+});
+
+const sendSuccess = (res, message, data = null) => sendResponse(res, 200, message, data);
+const sendCreated = (res, message, data = null) => sendResponse(res, 201, message, data);
+const sendBadRequest = (res, message = 'Bad Request') => sendError(res, message, 400);
+const sendNotFound = (res, message = 'Not Found') => sendError(res, message, 404);
+
+module.exports = {
+    sendResponse,
+    sendError,
+    sendSuccess,
+    sendCreated,
+    sendBadRequest,
+    sendNotFound,
+};
